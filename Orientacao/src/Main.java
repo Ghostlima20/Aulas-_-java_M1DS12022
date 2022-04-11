b595bb60d3c8672f1c8eb4c74c2b6aa9281ec445
aluno <aluno@DSN1251017048>
2022-04-11 10:52:18 -0300

    first commit

---
+++ Orientacao/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Pessoa pessoa1 = new Pessoa();
+		
+		pessoa1.nome= "webson";
+		pessoa1.numFigurinhas = 110;
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+		
+
+	}
+
+}
