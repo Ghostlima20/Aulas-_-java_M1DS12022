f111b8484c5d2e680f86287cfc67a87987e7ca2f
aluno <aluno@DSN1251017046>
2022-04-13 11:11:14 -0300

    second commit

--- Orientacao/src/Main.java
+++ Orientacao/src/Main.java
@@ -6,12 +6,32 @@ public class Main {
 		Pessoa pessoa1 = new Pessoa();
 		
 		pessoa1.nome= "webson";
-		pessoa1.numFigurinhas = 110;
+		pessoa1.numFigurinhas = 1;
+		
+		pessoa1.receber(3);
 		
 		System.out.println(pessoa1.nome);
 		System.out.println(pessoa1.numFigurinhas);
 		
-
+		Pessoa pessoa2 = new Pessoa();
+		
+		pessoa2.nome = "angela";
+		pessoa2.numFigurinhas = 5;
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);
+		
+		boolean msg = pessoa1.dar(4, pessoa2);
+		if(msg==true) {
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);
+		}
+		
 	}
 
+	
 }
+	
--- Orientacao/src/Pessoa.java
+++ Orientacao/src/Pessoa.java
@@ -1,16 +1,30 @@
 
 public class Pessoa {
-
 	String nome;
 	int numFigurinhas;
-	
-	void receber(int numFigurinhas) {
-		this.numFigurinhas = numFigurinhas;
-				
-}
 
-	void dar() {
-	
-}
+	void receber(int numFigurinhas) {
+		this.numFigurinhas += numFigurinhas;
+	}
+
+	boolean dar(int numFigurinhas,  Pessoa pessoa) {
+        
+		boolean msg; 
+		
+		this.numFigurinhas -= numFigurinhas;
+
+		pessoa.receber(numFigurinhas);
+		
+		if(this.numFigurinhas< numFigurinhas) {
+			System.out.println(" O numero de figurinhas insulficiente");
+			msg = false;
+				
+		} else {
+			this.numFigurinhas -= numFigurinhas;
+			pessoa.receber(numFigurinhas);
+			msg = true;
+		}
+	      return  msg;	
+	}
 	
 }
