c183afd27f0f29eadca32ed1eb79b63a04f9f4eb
aluno <aluno@DSN1251017048>
2022-04-27 11:38:54 -0300

    27/04/2022 lampadas

---
+++ exercicios/src/exercicios/Main.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Lampada n1 = new Lampada();
+		
+		n1.okLigada();
+		n1.imprimir();
+		n1.off();
+		n1.imprimir();
+	}
+
+}
