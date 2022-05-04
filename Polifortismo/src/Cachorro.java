f4d1a30c21347363f9d17945cb371e24deb3d15a
aluno <aluno@DSN1251017042>
2022-05-04 11:32:36 -0300

    first

---
+++ Polifortismo/src/Cachorro.java
@@ -0,0 +1,18 @@
+
+public class Cachorro extends Animal {
+
+	@Override
+	public void Falar() {
+		
+		System.out.println(" AU AU AU ");	
+}
+
+	public void Morder() {
+		
+		System.out.println("CARACA BOLACHA!!");
+	}
+	
+	
+
+
+}
