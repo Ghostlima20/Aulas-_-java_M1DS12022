ef8f8b2362b8d59175f74d0d4ee4d6a5b686d6e7
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 09:38:58 -0300

    Update Calculadora3.java

--- Calculadora/src/Calculadora3.java
+++ Calculadora/src/Calculadora3.java
@@ -13,7 +13,7 @@ public class Calculadora3 {
 	
 		Scanner entrada = new Scanner(System.in);
 		
-		// Interacao com o usuario
+		// Interagindo com o usuario
 		System.out.println("Digite a operação desejada (+,-,*,/)");
 		op=entrada.nextLine();
 		
@@ -23,7 +23,7 @@ public class Calculadora3 {
 		System.out.println("Digite um segundo valor: ");
 		n2=Integer.parseInt(entrada.nextLine());
 		
-		// Validacao das contas de acordo com o que o usuario digitou
+		// certificando as contas de acordo com o que o usuario digitou.
 		if(op.equals("+")) {
 			System.out.println("O resultado da soma é: "+(n1+n2));
 			control++;
