d7adca4fc6757cac5f3d608486dfea9028d9989e
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 10:03:43 -0300

    Update Aplicacao.java

--- ExercicioAreaCalculavel/src/Aplicacao.java
+++ ExercicioAreaCalculavel/src/Aplicacao.java
@@ -2,7 +2,7 @@
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		//Aqui foi instanciado os objetos de cada tipo de classe para interagir com os metodos delas e mostrar para o usurio a area dos poligonos que ele desejar
+		//instanciado os objetos de cada tipo de classe para interagir com os metodos delas.
 		Retangulo retangulo = new Retangulo();
 		retangulo.x=8;
 		retangulo.y=10;
