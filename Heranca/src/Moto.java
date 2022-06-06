5ba8a064d35a8f5820f5874a3db21bd0550a68cc
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 08:58:11 -0300

    Delete Heranca/src directory

--- Heranca/src/Moto.java
+++
@@ -1,8 +0,0 @@
-// Moto tambem extende de veiculo
-public class Moto extends Veiculo {
-	// Aqui tem um metodo a mais que o de empinar
-	public void empinar(){
-		// Mensagem verdadeira, peco perdao pela sinceridade
-		System.out.println("EMPINAR É COISA DE OTÁRIO");
-	}
-}
