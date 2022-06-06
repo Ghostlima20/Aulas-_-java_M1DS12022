f78c5ec9529e1263b79f1996a84ebc78c6eb90c4
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 10:04:51 -0300

    Update AreaCalculavel.java

--- ExercicioAreaCalculavel/src/AreaCalculavel.java
+++ ExercicioAreaCalculavel/src/AreaCalculavel.java
@@ -1,6 +1,6 @@
-// Area calculavel e uma interface que vai receber um metodo generico
+// Ainetrface ira receber o generico
 public interface AreaCalculavel {
-	// O metodo generico dessa interface sera melhor definido nas classes que o utilizarem
+	// ira ser melhor compreendido nas outras classes.
 	public double calcularArea();
 
 }
