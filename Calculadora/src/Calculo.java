cf9f79e567d8b72e4876ca7f9e60eeada91259ca
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 09:42:26 -0300

    Update Calculo.java

--- Calculadora/src/Calculo.java
+++ Calculadora/src/Calculo.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Calculo {
-	// Projeto feitio por conta no primeiro dia
+	// Projeto realizado por conta.
 	public static void main (String[]args) {
 		Scanner entrada = new Scanner (System.in);
 		// Criando variaveis
@@ -15,24 +15,24 @@ public class Calculo {
 		int divisao=0;
 		int continua=1;
 		
-		// While utilizado para permitir que a pessoa faca quantas contas ela desejar ate digitar que nao queira mais
+		// loop utilizado para permitir que a pessoa faca quantas contas ela desejar ate quando ela desejar sair
 		while(continua==1) {
 		System.out.print("Selecione a operação desejada: 1-Soma 2-Subtração 3-Multiplicação 4-Divisão \n");
 		operacao=entrada.nextInt();
 		
-		// Validacao para permitir que a pessoa digite apenas valores validos (1 a 4)
+		// usuario so pode digitar numeros validos 0 a 4
 		if(operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4) {
 			System.out.printf("Não é possível realizar essa operação \n");
 		}
 		else {
 		
-		// Interacao com o usuario
+		// intergaindo com o usuario
 		System.out.print("Digite o primeiro número: \n");
 		numero1= entrada.nextInt();
 		System.out.print("Digite o segundo número: \n");
 		numero2= entrada.nextInt();
 		
-		// Contas de acordo com o que a pessoa escolheu
+		// Contas que o usuario escolheu.
 		if(operacao == 1) {
 		soma=numero1+numero2;
 		System.out.printf("A soma é: %d\n", soma);
@@ -53,7 +53,7 @@ public class Calculo {
 			System.out.printf("A divisão é: %d\n", divisao);
 			}
 		
-		// Validacao se a pessoa quer mais contas ou nao
+		// Validacao  se o usuario desejar mais contas.
 		System.out.println("\n Deseja realizar mais algum cálculo? 1-Sim 2-Não");
 		continua=entrada.nextInt();
 		
