0bc35911a139855719c7361cd3b23f282d9d014a
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 09:43:21 -0300

    Update Aplicacao.java

--- ClassesAbstratas/src/Aplicacao.java
+++ ClassesAbstratas/src/Aplicacao.java
@@ -2,7 +2,7 @@
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// Item item = new Item();  Isso d· errado porque n„o podemos instanciar um objeto abstrato
+		// Item item = new Item(); nao permite pois n√£o podemos instanciar um objeto abstrato
 		
 		Item item = new Cogumelo();
 		item.pegar();
