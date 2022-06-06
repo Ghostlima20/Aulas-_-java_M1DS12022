6121d9176b7da72206ef306f4e3366963bbac807
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 10:09:10 -0300

    Update Aplicacao.java

--- ExercicioContaBancaria/Aplicacao.java
+++ ExercicioContaBancaria/Aplicacao.java
@@ -8,25 +8,25 @@ public class Aplicacao {
 		ContaBancaria ContaInvestimento = new ContaInvestimento();
 		
 
-		System.out.printf("O saldo na conta corrente � equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
-		System.out.printf("O saldo na conta investimento � equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		System.out.printf("O saldo na conta corrente  equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento  equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
 		
 		
 		ContaCorrente.depositar(200);
 		ContaInvestimento.depositar(200);
-		System.out.printf("O saldo na conta corrente � equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
-		System.out.printf("O saldo na conta investimento � equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		System.out.printf("O saldo na conta corrente equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento  equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
 		
 		
 		ContaCorrente.sacar(100);
 		ContaInvestimento.sacar(100);
-		System.out.printf("O saldo na conta corrente � equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
-		System.out.printf("O saldo na conta investimento � equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		System.out.printf("O saldo na conta corrente equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
 		
 		
 		ContaInvestimento.trasnferir(200, ContaCorrente);
-		System.out.printf("O saldo na conta corrente � equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
-		System.out.printf("O saldo na conta investimento � equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		System.out.printf("O saldo na conta corrente  equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
 	}
 
 }
