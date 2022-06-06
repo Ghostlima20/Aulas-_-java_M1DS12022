b51547b61259184450ae70aba62d8b1aa2413bcc
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 10:08:09 -0300

    Update testeGit.java

--- testeGit/src/testeGit/testeGit.java
+++ testeGit/src/testeGit/testeGit.java
@@ -3,8 +3,8 @@ package testeGit;
 public class testeGit {
 
 	public static void main(String[] args) {
-		// Aqui foi apenas um teste para ver se o git funcionava. Nada de mais ^_____^
-		System.out.println("Pedro H S Torini");
+		//teste para funcionamento
+		System.out.println("PALMEIRAS NAO TEM MUNDIAL");
 	}
 
 }
