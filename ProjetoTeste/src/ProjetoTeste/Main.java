c374b10b144918280d3d5ac85429cb3749575b44
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-06 08:59:13 -0300

    Delete ProjetoTeste/src/ProjetoTeste directory

--- ProjetoTeste/src/ProjetoTeste/Main.java
+++
@@ -1,9 +0,0 @@
-package ProjetoTeste;
-
-public class Main {
-	public static void main(String[] args) {
-		// Aqui se deu o inicio do processo mais importante para se tornar um programador de respeito, printar Hello World na tela
-		System.out.println("Hello World");
-		// TODO Auto-generated method stub
-	}
-}
--- ProjetoTeste/src/ProjetoTeste/New.java
+++
@@ -1,8 +0,0 @@
-package ProjetoTeste;
-
-public class New {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		// TODO Auto-generated method stub
-	}
-}
