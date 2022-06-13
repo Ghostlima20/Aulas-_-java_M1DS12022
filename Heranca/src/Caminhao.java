12d25fa7c7768788a3853f35c5fd8750ae8493da
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-13 08:48:01 -0300

    Update Caminhao.java

--- Heranca/src/Caminhao.java
+++ Heranca/src/Caminhao.java
@@ -1,9 +1,9 @@
 // exntends mostra que a classe Caminhao herda de veiculo
 public class Caminhao extends Veiculo{
-	// Aqui foi sobrescrito o metodo buzinar, pois a buzina do caminhao e diferente do padrao definido
+	//  metodo buzinar, pois a buzina do caminhao e diferente 
 	@Override
 	public void Buzinar() {
-		System.out.println("FON FON");
+		System.out.println("ai pai para");
 	}
 	
 	
