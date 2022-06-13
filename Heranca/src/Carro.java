f8fef6e35d560fb56a60a1398059f2195567e034
Ghostlima20 <102963734+Ghostlima20@users.noreply.github.com>
2022-06-13 08:45:28 -0300

    Update Carro.java

--- Heranca/src/Carro.java
+++ Heranca/src/Carro.java
@@ -3,12 +3,12 @@ public class Carro extends Veiculo { //Extends coloca a classe como filha
 	// Variavel 
 	private boolean quatroPortas;
 	
-	// Aqui sobrescrevemos o metodo Imprimir, pois o carro possui uma variavel a mais que os outros nao vao ter
+	// sobrescrever o metodo Imprimir
 	@Override
 	public void Imprimir() {
-		// super herda todo o codigo que esta dentro do metodo imprimir 
+		// super herda 
 		super.Imprimir();
-		// aqui foi adicionado algo a mais que no caso e a definicao se o carro tem ou nao quatro portas
+		// mensagem para usuario
 		System.out.println("Tem quatro portas? " + quatroPortas);
 	}
 
